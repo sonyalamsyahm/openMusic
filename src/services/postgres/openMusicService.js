a1ce27b4c8f180418e4ea58dac0c7bcc3e0b63cd
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-12 10:07:15 +0700

    add openMusic service

---
+++ src/services/postgres/openMusicService.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-underscore-dangle */
+const nanoid = require('nanoid');
+const { Pool } = require('pg');
+const { mapDBToModel } = require('../../utils');
+const InvariantError = require('../../exception/InvariantError');
+const NotFoundError = require('../../exception/NotFoundError');
+
+class openMusicService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addMusic({
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO musics VALUES=($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getMusics() {
+    const result = await this._pool.query('SELECT * FROM musics');
+    return result.rows.map(mapDBToModel); // PERLU PENYESUAIAN MAPDBToModel nya agar sesuai kriteria
+  }
+
+  async getMusicById(id) {
+    const query = {
+      text: 'SELECT * FROM musics WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editMusicById(id, {
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE musics SET title = $1, year = $2, performer = $3, genre = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id',
+      values: [title, year, performer, genre, duration, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+  }
+
+  async deleteMusicById(id) {
+    const query = {
+      text: 'DELETE FROM musics WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = openMusicService;
