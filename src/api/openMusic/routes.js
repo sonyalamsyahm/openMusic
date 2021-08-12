4211b7270ae5c811dd6437f546fff9951fe9476d
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-12 10:05:55 +0700

    init api

---
+++ src/api/openMusic/routes.js
@@ -0,0 +1,29 @@
+const routes = () => [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/songs/{songId}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{songId}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{songId}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
