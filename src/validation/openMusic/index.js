9242623cbf6c6b375ed85570c71a0cd99bdcb171
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-12 16:52:50 +0700

    song data validator added

---
+++ src/validation/openMusic/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exception/InvariantError');
+const { songPayloadSchema } = require('./schema');
+
+const songValidator = {
+  validateSongPayload: (payload) => {
+    const validationResult = songPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = songValidator;
