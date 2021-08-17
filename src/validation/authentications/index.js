79280bff05c481a44d87240db1e6da1e144f5947
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-17 13:56:46 +0700

    login as authentication validator paylaod added

---
+++ src/validation/authentications/index.js
@@ -0,0 +1,14 @@
+const { PostAuthenticationPayloadSchema } = require('./schema');
+const InvariantError = require('../../exception/InvariantError');
+
+const authenticationValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = { authenticationValidator };
