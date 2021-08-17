dc13511c96fecd21d75ed7eab86fd1c303e5c400
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-17 13:42:05 +0700

    validate song's year

--- src/validation/openMusic/schema.js
+++ src/validation/openMusic/schema.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 const songPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(2021)
+    .required(),
   performer: Joi.string().required(),
   genre: Joi.string().required(),
   duration: Joi.number().required(),
