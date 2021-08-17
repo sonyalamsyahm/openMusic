400d5f4b5a32dc2d279e27d5f87ec2d513a6b00b
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-17 19:43:37 +0700

    lifecycle onPreResp to handling err, plugin auth added

--- src/server.js
+++ src/server.js
@@ -3,19 +3,31 @@ const Hapi = require('@hapi/hapi');
 
 // openMusic
 const openMusic = require('./api/openMusic');
-const OpenMusicService = require('./services/postgres/openMusicService');
-const openMusicValidator = require('./validation/openMusic');
+const OpenMusicService = require('./services/postgres/OpenMusicService');
+const OpenMusicValidator = require('./validation/openMusic');
 
 // users
 const users = require('./api/users');
-const UsersService = require('./services/postgres/usersService');
+const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validation/users');
+
+// authentication
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validation/authentications');
+
+// tokenize
+const TokenManager = require('./tokenize/TokenManager');
+
+// error
+const ClientError = require('./exception/ClientError');
 
 require('dotenv').config();
 
 const init = async () => {
   const openMusicService = new OpenMusicService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.Server({
     port: process.env.PORT,
@@ -32,7 +44,7 @@ const init = async () => {
       plugin: openMusic,
       options: {
         service: openMusicService,
-        validator: openMusicValidator,
+        validator: OpenMusicValidator,
       },
     },
     {
@@ -42,7 +54,44 @@ const init = async () => {
         validator: UsersValidator,
       },
     },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
   ]);
+
+  // use hapi lifecycle to handling Error
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    let newResponse = null;
+    if (response instanceof ClientError) {
+      newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+    } else if (response instanceof Error) {
+      newResponse = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+
+      newResponse.code(500);
+      console.error(response);
+    }
+
+    if (newResponse) {
+      return newResponse;
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
