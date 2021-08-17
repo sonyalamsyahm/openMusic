34cbb33c78c344c2834a70184a335d169e5679a8
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-17 16:25:49 +0700

    plugin users implemented

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-console */
 const Hapi = require('@hapi/hapi');
+
+// openMusic
 const openMusic = require('./api/openMusic');
 const OpenMusicService = require('./services/postgres/openMusicService');
 const openMusicValidator = require('./validation/openMusic');
 
-require('dotenv').config();
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/usersService');
+const UsersValidator = require('./validation/users');
 
-// Config dalam .env merupakan config development dimana HOST masih localhost
-// Dalam submission tidak dicantumkan untuk membuat config untuk production jadi tidak saya buat
-// Saya lampiran file .env untuk memastikan kesesuian dengan kriteria submission
+require('dotenv').config();
 
 const init = async () => {
   const openMusicService = new OpenMusicService();
+  const usersService = new UsersService();
 
   const server = Hapi.Server({
     port: process.env.PORT,
@@ -23,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: openMusic,
-    options: {
-      service: openMusicService,
-      validator: openMusicValidator,
+  await server.register([
+    {
+      plugin: openMusic,
+      options: {
+        service: openMusicService,
+        validator: openMusicValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
