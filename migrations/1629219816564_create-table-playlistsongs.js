09f60c260b03a3cb7f030c9aefd73ed554ad666b
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-18 01:01:15 +0700

    migration playlistsongs table added

---
+++ migrations/1629219816564_create-table-playlistsongs.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlistsongs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  // unique playlist & song_id to prevent multiple data
+  pgm.addConstraint('playlistsongs', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)');
+
+  // add constraint foreign key
+  pgm.addConstraint('playlistsongs', 'fk_playlistsongs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlistsongs', 'fk_playlistsongs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlistsongs');
+};
