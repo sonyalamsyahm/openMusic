26f312dfc06c6569f49be0574a07bf2ae1be154a
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-17 19:40:40 +0700

    remove error handler at users handler

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,5 +1,5 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["_service", _validator] }] */
-const ClientError = require('../../exception/ClientError');
+// const ClientError = require('../../exception/ClientError');
 
 class UsersHandler {
   constructor(service, validator) {
@@ -10,39 +10,17 @@ class UsersHandler {
   }
 
   async postUserHandler(request, h) {
-    try {
-      this._validator.validateUserPayload(request.payload);
-      const { username, password, fullname } = request.payload;
-      const userId = await this._service.addUser({ username, password, fullname });
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+    const userId = await this._service.addUser({ username, password, fullname });
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: { userId },
+    });
 
-      const response = h.response({
-        status: 'success',
-        message: 'User berhasil ditambahkan',
-        data: { userId },
-      });
-
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-
-        response.code(error.statusCode);
-        return response;
-      }
-
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    response.code(201);
+    return response;
   }
 }
 
