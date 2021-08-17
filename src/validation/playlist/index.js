11bc5836272a6be502320bca5f0af6b8b62e0f40
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-18 01:03:05 +0700

    validate Post playlist adding songs

--- src/validation/playlist/index.js
+++ src/validation/playlist/index.js
@@ -1,9 +1,16 @@
-const { PlaylistPayloadSchema } = require('./schema');
+const { PlaylistPayloadSchema, PlaylistAddSongPayloadSchema } = require('./schema');
 const InvariantError = require('../../exception/InvariantError');
 
 const PlaylistsValidator = {
   PostPlaylistValidator: (payload) => {
     const validationResult = PlaylistPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  PostPlaylistAddSongValidator: (payload) => {
+    const validationResult = PlaylistAddSongPayloadSchema.validate(payload);
 
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
